52ffdcba596859b0b6f72eb9b288fbdb311b3b8e
Oliver Letterer <oliver.letterer@gmail.com>
2013-05-03 18:01:59 +0200

    xctool.

--- test.sh
+++ test.sh
@@ -4,8 +4,7 @@ git clone https://github.com/OliverLetterer/xctool.git /tmp/xctool
 
 me=$(whoami)
 cp -r SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/xcuserdata/oliver.xcuserdatad/* SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/xcuserdata/$me.xcuserdatad
-
-ls -la SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/xcuserdata
+cp -r SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/project.xcworkspace/xcuserdata/oliver.xcuserdatad/* SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/project.xcworkspace/xcuserdata/$me.xcuserdatad
 
 runTest() {
 	/tmp/xctool/xctool.sh -project "SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj" -scheme "SLRESTfulCoreData" -configuration "$1" test -test-sdk "$2"
