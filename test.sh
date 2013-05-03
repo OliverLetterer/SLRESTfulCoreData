047e5fe40e2a67462165c47046e0a8db63deea55
Oliver Letterer <oliver.letterer@gmail.com>
2013-05-03 07:59:10 +0200

    Migrates unit tests to xctool.

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git clone https://github.com/facebook/xctool.git /tmp/xctool
+
+runTest() {
+	/tmp/xctool/xctool.sh -project ./SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj -scheme SLRESTfulCoreData -configuration $1 test -test-sdk $2
+	if [ $? != 0 ]; then 
+		exit 1
+	fi
+}
+
+runTest "Debug" "iphonesimulator5.1"
+runTest "Release" "iphonesimulator5.1"
+runTest "Debug" "iphonesimulator6.1"
+runTest "Release" "iphonesimulator6.1"
+
+rm -rf /tmp/xctool
