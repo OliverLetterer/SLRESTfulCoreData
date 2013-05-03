085d0b56620f3de7797f8baf76a3d44f7a88df23
Oliver Letterer <oliver.letterer@gmail.com>
2013-05-03 19:02:23 +0200

    xctool.

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+printf "\e[32m Installing xctool...\n"
 git clone https://github.com/facebook/xctool.git /tmp/xctool >/dev/null
+
+if [ $? != 0 ]; then 
+	printf "\e[31m Cloning xctool failed"
+	exit 1
+fi
 
 me=$(whoami)
 
@@ -12,12 +18,11 @@ cp -r SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj/xcuserdata/oliver.xcuserdata
 runTest() {
 	/tmp/xctool/xctool.sh -project "SLRESTfulCoreData/SLRESTfulCoreData.xcodeproj" -scheme "SLRESTfulCoreData" -configuration "$1" test -test-sdk "$2"
 	if [ $? != 0 ]; then 
+		printf "\e[31m Test with SDK $2 failed"
 		exit 1
 	fi
 }
 
-runTest "Debug"   "iphonesimulator5.1"
 runTest "Release" "iphonesimulator5.1"
-runTest "Debug"   "iphonesimulator6.1"
 runTest "Release" "iphonesimulator6.1"
 
